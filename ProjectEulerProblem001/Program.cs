8e6f053bdc52c9ff52167ebaf7a4a174cad2746a
beckerd8 <beckerd8@LCBLAB2110-29>
2024-10-24 11:31:34 -0400

    Added documentation and intro.

--- ProjectEulerProblem001/Program.cs
+++ ProjectEulerProblem001/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+* Name: David Becker
+* email:  Beckerd8@mail.uc.edu
+* Assignment Number: In Class GitHub Learning
+* Due Date:   10/24/2024
+* Course #/Section:   IS3050-001
+* Semester/Year:   Fall 2024
+* Brief Description of the assignment:  Learning the basic behind GitHub (fork, clone, commit, push, repo, pull request)
+* 
+* Brief Description of what this module does: Tests GitHub
+* Citations:
+* Anything else that's relevant: 
+* Fork: Copy from my repo into yours
+* Clone: makes a copy of your remote repo onto your desktop
+* Commit and Push
+* Commit = save your changes to your local repo
+* Push = sends the local repo to github
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
