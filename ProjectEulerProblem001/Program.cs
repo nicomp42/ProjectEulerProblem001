d663d256b75a068c0d3143ece232d3c45d8d7502
Derick-Bellofatto <Bellofdk@mail.uc.edu>
2024-10-24 11:32:06 -0400

    Updated my assignment documentation at the top of the file

--- ProjectEulerProblem001/Program.cs
+++ ProjectEulerProblem001/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+*Name: Derick Bellofatto
+*email:  Bellofdk@mail.uc.edu
+*Assignment Number: Assignment InClass10/24/2024
+*Due Date: 10/24/2024
+*Course #/Section:   IS3050-001
+* Semester / Year:   Fall 2024
+*Brief Description of the assignment:  Make changes to a repository off github
+
+*Brief Description of what this module does: Module itself finds all multiples of 3 and 5 below 1000 and adds them together
+*Citations: Original Document ~ https://github.com/nicomp42/ProjectEulerProblem001
+*Anything else that's relevant:
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
