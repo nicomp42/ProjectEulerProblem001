902b0153b3c6f6e8c2bb95ee48f95ac11374c0c8
Shantele King <king4sl@mail.uc.edu>
2024-10-24 11:32:14 -0400

    Added documentaion for in class work.

--- ProjectEulerProblem001/Program.cs
+++ ProjectEulerProblem001/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/* Name: Shantele King
+# email: King4sl@mail.uc.edu 
+# Assignment Title: InClass10/24/2024
+# Due Date: 10/224/2024
+# Course: IS 3050
+# Semester/Year: Fall 2024
+# Brief Description:C# Practice adding in data using a fork from github and making a clone
+# Citations:N/A
+# Anything else that's relevant: N/A 
+*/
+//do a commit which saves changes to local repo//
+//Push will send the local repo...
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
